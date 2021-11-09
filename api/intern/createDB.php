2165bebfb156dbb30166ac59b8e5e5ebb370bd73
supertoll <89698655+supertoll@users.noreply.github.com>
2021-11-09 15:41:58 +0100

    update

--- api/intern/createDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "database.php";
-$db = new Database("localhost","root","");
-$db->connect();
-$db->executeSQLFromFile("./../DB/createLunchBoxFoodDB.sql");
-$db->disconnect();
-?>
--- api/intern/dropDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "database.php";
-$db = new Database("localhost","root","");
-$db->connect();
-$db->dropDB();
-$db->disconnect();
-?>

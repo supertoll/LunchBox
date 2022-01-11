f2389cb1363df70e18adb99b61ace39cfc1aa04d
supertoll <89698655+supertoll@users.noreply.github.com>
2022-01-11 18:23:28 +0100

    change acces --> *

--- api/delRating/index.php
+++ api/delRating/index.php
@@ -5,7 +5,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: DELETE");
 
 #? ToDo check what mean
-header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Credentials: true");
 
 # using fooddb class

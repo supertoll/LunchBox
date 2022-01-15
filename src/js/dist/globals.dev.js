499e63bfad0aa6e4b3b78e3425f09d721fdfa6a7
florian <f.code1000@gmail.com>
2022-01-16 00:06:00 +0100

    sever fix

--- src/js/dist/globals.dev.js
+++ src/js/dist/globals.dev.js
@@ -39,19 +39,30 @@ var global = {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
+            _context.prev = 0;
+            _context.next = 3;
             return regeneratorRuntime.awrap(_framework.request.get(webserver + 'getLocations/index.php'));
 
-          case 2:
+          case 3:
             x = _context.sent;
             locations = JSON.parse(x.data);
+            _context.next = 11;
+            break;
 
-          case 4:
+          case 7:
+            _context.prev = 7;
+            _context.t0 = _context["catch"](0);
+            locations = {
+              "locations": ["Berlin Springpfuhl", "Neubrandenburg"]
+            };
+            console.log(locations);
+
+          case 11:
           case "end":
             return _context.stop();
         }
       }
-    });
+    }, null, null, [[0, 7]]);
   },
   getLocations: function getLocations() {
     return locations;
@@ -74,83 +85,81 @@ var global = {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
-            console.log(webserver + 'getOffer/?date=' + d + '&location=["' + location + '"]');
+            _context2.prev = 0;
             _context2.next = 3;
             return regeneratorRuntime.awrap(_framework.request.get(webserver + 'getOffer/?date=' + d + '&location=["' + location + '"]'));
 
           case 3:
             x = _context2.sent;
-            offers = JSON.parse(x.data); //    offers = [
-            //        {
-            //            "id": 696,
-            //            "providerId": 3,
-            //            "name": "Eier",
-            //            "description": "in süß- saurer Soße mit Kartoffeln, dazu Rohkost",
-            //            "price": null,
-            //            "averageRating": 5,
-            //            "tags": [],
-            //            "comments": []
-            //        },
-            //        {
-            //            "id": 697,
-            //            "providerId": 3,
-            //            "name": "Hähnchenschnitzel",
-            //            "description": "mit Mischgemüse und Kartoffeln",
-            //            "price": null,
-            //            "averageRating": 2,
-            //            "tags": [],
-            //            "comments": []
-            //        },
-            //        {
-            //            "id": 698,
-            //            "providerId": 3,
-            //            "name": "gebratenes Zanderfilet",
-            //            "description": "mit Kaisergemüse und Püree",
-            //            "price": null,
-            //            "averageRating": 1.5,
-            //            "tags": [],
-            //            "comments": []
-            //        },
-            //        {
-            //            "id": 724,
-            //            "providerId": 10,
-            //            "name": "mit Backpflaumen gefüllter Schweinebraten,",
-            //            "description": "dazu Rotkohl und Knödelscheiben",
-            //            "price": 650,
-            //            "averageRating": 3.5,
-            //            "tags": [],
-            //            "comments": []
-            //        },
-            //        {
-            //            "id": 728,
-            //            "providerId": 4,
-            //            "name": "Pasta „Pomodori“",
-            //            "description": "frische Tomaten, Parmesan, Olivenöl, Basilikum, Hühnchenbrust, dazu Nudeln",
-            //            "price": 520,
-            //            "averageRating": null,
-            //            "tags": [
-            //                "Tagessuppe"
-            //            ],
-            //            "comments": []
-            //        },
-            //        {
-            //            "id": 733,
-            //            "providerId": 4,
-            //            "name": "Präsidentensuppe",
-            //            "description": "Rinderhack, Tomaten, Sauerkraut, saure Gurken, Tomatenmark, wahlweise + Schmand",
-            //            "price": 520,
-            //            "averageRating": null,
-            //            "tags": [],
-            //            "comments": []
-            //        }
-            //    ]
+            offers = JSON.parse(x.data);
+            _context2.next = 10;
+            break;
 
-          case 5:
+          case 7:
+            _context2.prev = 7;
+            _context2.t0 = _context2["catch"](0);
+            offers = [{
+              "id": 696,
+              "providerId": 3,
+              "name": "Eier",
+              "description": "in süß- saurer Soße mit Kartoffeln, dazu Rohkost",
+              "price": null,
+              "averageRating": 5,
+              "tags": [],
+              "comments": []
+            }, {
+              "id": 697,
+              "providerId": 3,
+              "name": "Hähnchenschnitzel",
+              "description": "mit Mischgemüse und Kartoffeln",
+              "price": null,
+              "averageRating": 2,
+              "tags": [],
+              "comments": []
+            }, {
+              "id": 698,
+              "providerId": 3,
+              "name": "gebratenes Zanderfilet",
+              "description": "mit Kaisergemüse und Püree",
+              "price": null,
+              "averageRating": 1.5,
+              "tags": [],
+              "comments": []
+            }, {
+              "id": 724,
+              "providerId": 10,
+              "name": "mit Backpflaumen gefüllter Schweinebraten,",
+              "description": "dazu Rotkohl und Knödelscheiben",
+              "price": 650,
+              "averageRating": 3.5,
+              "tags": [],
+              "comments": []
+            }, {
+              "id": 728,
+              "providerId": 4,
+              "name": "Pasta „Pomodori“",
+              "description": "frische Tomaten, Parmesan, Olivenöl, Basilikum, Hühnchenbrust, dazu Nudeln",
+              "price": 520,
+              "averageRating": null,
+              "tags": ["Tagessuppe"],
+              "comments": []
+            }, {
+              "id": 733,
+              "providerId": 4,
+              "name": "Präsidentensuppe",
+              "description": "Rinderhack, Tomaten, Sauerkraut, saure Gurken, Tomatenmark, wahlweise + Schmand",
+              "price": 520,
+              "averageRating": null,
+              "tags": [],
+              "comments": []
+            }];
+
+          case 10:
           case "end":
             return _context2.stop();
         }
       }
-    });
+    }, null, null, [[0, 7]]);
   },
   initProviders: function initProviders() {
     var x;
@@ -158,55 +167,57 @@ var global = {
       while (1) {
         switch (_context3.prev = _context3.next) {
           case 0:
-            _context3.next = 2;
+            _context3.prev = 0;
+            _context3.next = 3;
             return regeneratorRuntime.awrap(_framework.request.get(webserver + 'getProvider/?location=["' + location + '"]'));
 
-          case 2:
+          case 3:
             x = _context3.sent;
-            providers = JSON.parse(x.data); //        providers = [
-            //            {
-            //                "id": 1,
-            //                "name": "Schweinestall",
-            //                "location": "Neubrandenburg",
-            //                "url": "https://www.schweinestall-nb.de/mittagstisch-2/"
-            //            },
-            //            {
-            //                "id": 2,
-            //                "name": "Hotel am Ring",
-            //                "location": "Neubrandenburg",
-            //                "url": "http://www.hotel-am-ring.de/restaurant-rethra.html"
-            //            },
-            //            {
-            //                "id": 3,
-            //                "name": "AOK Cafeteria",
-            //                "location": "Neubrandenburg",
-            //                "url": "https://www.tfa-bistro.de"
-            //            },
-            //            {
-            //                "id": 4,
-            //                "name": "Suppenkulttour",
-            //                "location": "Neubrandenburg",
-            //                "url": "https://www.suppenkult.com/wochenplan.html"
-            //            },
-            //            {
-            //                "id": 8,
-            //                "name": "Das Krauthof",
-            //                "location": "Neubrandenburg",
-            //                "url": "https://www.daskrauthof.de/karte"
-            //            },
-            //            {
-            //                "id": 10,
-            //                "name": "Phoenixeum",
-            //                "location": "Neubrandenburg",
-            //                "url": "https://www.suppenkult.com/wochenplan.html"
-            //            }
+            providers = JSON.parse(x.data);
+            _context3.next = 10;
+            break;
 
-          case 4:
+          case 7:
+            _context3.prev = 7;
+            _context3.t0 = _context3["catch"](0);
+            providers = [{
+              "id": 1,
+              "name": "Schweinestall",
+              "location": "Neubrandenburg",
+              "url": "https://www.schweinestall-nb.de/mittagstisch-2/"
+            }, {
+              "id": 2,
+              "name": "Hotel am Ring",
+              "location": "Neubrandenburg",
+              "url": "http://www.hotel-am-ring.de/restaurant-rethra.html"
+            }, {
+              "id": 3,
+              "name": "AOK Cafeteria",
+              "location": "Neubrandenburg",
+              "url": "https://www.tfa-bistro.de"
+            }, {
+              "id": 4,
+              "name": "Suppenkulttour",
+              "location": "Neubrandenburg",
+              "url": "https://www.suppenkult.com/wochenplan.html"
+            }, {
+              "id": 8,
+              "name": "Das Krauthof",
+              "location": "Neubrandenburg",
+              "url": "https://www.daskrauthof.de/karte"
+            }, {
+              "id": 10,
+              "name": "Phoenixeum",
+              "location": "Neubrandenburg",
+              "url": "https://www.suppenkult.com/wochenplan.html"
+            }];
+
+          case 10:
           case "end":
             return _context3.stop();
         }
       }
-    });
+    }, null, null, [[0, 7]]);
   },
   getOffers: function getOffers() {
     return offers;
--- src/js/globals.js
+++ src/js/globals.js
@@ -20,8 +20,19 @@ const global = {
         return id;
     },
     initLocation: async () =>{
-        var x = await request.get(webserver + 'getLocations/index.php')
-        locations = JSON.parse(x.data)
+        try{
+            var x = await request.get(webserver + 'getLocations/index.php')
+            locations = JSON.parse(x.data)
+        }
+        catch{
+            locations = {
+                "locations":[
+                    "Berlin Springpfuhl",
+                    "Neubrandenburg"
+                ]
+            }
+            console.log(locations)
+        }
     },
     getLocations: () =>{
         return locations;
@@ -39,116 +50,122 @@ const global = {
         location = l;
     },
     initOffers: async (d) =>{
-        console.log(webserver + 'getOffer/?date=' + d + '&location=["' + location + '"]');
-        var x = await request.get(webserver + 'getOffer/?date=' + d + '&location=["' + location + '"]');
-
-        offers = JSON.parse(x.data);
-
-    //    offers = [
-    //        {
-    //            "id": 696,
-    //            "providerId": 3,
-    //            "name": "Eier",
-    //            "description": "in süß- saurer Soße mit Kartoffeln, dazu Rohkost",
-    //            "price": null,
-    //            "averageRating": 5,
-    //            "tags": [],
-    //            "comments": []
-    //        },
-    //        {
-    //            "id": 697,
-    //            "providerId": 3,
-    //            "name": "Hähnchenschnitzel",
-    //            "description": "mit Mischgemüse und Kartoffeln",
-    //            "price": null,
-    //            "averageRating": 2,
-    //            "tags": [],
-    //            "comments": []
-    //        },
-    //        {
-    //            "id": 698,
-    //            "providerId": 3,
-    //            "name": "gebratenes Zanderfilet",
-    //            "description": "mit Kaisergemüse und Püree",
-    //            "price": null,
-    //            "averageRating": 1.5,
-    //            "tags": [],
-    //            "comments": []
-    //        },
-    //        {
-    //            "id": 724,
-    //            "providerId": 10,
-    //            "name": "mit Backpflaumen gefüllter Schweinebraten,",
-    //            "description": "dazu Rotkohl und Knödelscheiben",
-    //            "price": 650,
-    //            "averageRating": 3.5,
-    //            "tags": [],
-    //            "comments": []
-    //        },
-    //        {
-    //            "id": 728,
-    //            "providerId": 4,
-    //            "name": "Pasta „Pomodori“",
-    //            "description": "frische Tomaten, Parmesan, Olivenöl, Basilikum, Hühnchenbrust, dazu Nudeln",
-    //            "price": 520,
-    //            "averageRating": null,
-    //            "tags": [
-    //                "Tagessuppe"
-    //            ],
-    //            "comments": []
-    //        },
-    //        {
-    //            "id": 733,
-    //            "providerId": 4,
-    //            "name": "Präsidentensuppe",
-    //            "description": "Rinderhack, Tomaten, Sauerkraut, saure Gurken, Tomatenmark, wahlweise + Schmand",
-    //            "price": 520,
-    //            "averageRating": null,
-    //            "tags": [],
-    //            "comments": []
-    //        }
-    //    ]
+        try{
+            var x = await request.get(webserver + 'getOffer/?date=' + d + '&location=["' + location + '"]');
+            offers = JSON.parse(x.data);
+        }
+        catch{
+            offers = [
+                {
+                    "id": 696,
+                    "providerId": 3,
+                    "name": "Eier",
+                    "description": "in süß- saurer Soße mit Kartoffeln, dazu Rohkost",
+                    "price": null,
+                    "averageRating": 5,
+                    "tags": [],
+                    "comments": []
+                },
+                {
+                    "id": 697,
+                    "providerId": 3,
+                    "name": "Hähnchenschnitzel",
+                    "description": "mit Mischgemüse und Kartoffeln",
+                    "price": null,
+                    "averageRating": 2,
+                    "tags": [],
+                    "comments": []
+                },
+                {
+                    "id": 698,
+                    "providerId": 3,
+                    "name": "gebratenes Zanderfilet",
+                    "description": "mit Kaisergemüse und Püree",
+                    "price": null,
+                    "averageRating": 1.5,
+                    "tags": [],
+                    "comments": []
+                },
+                {
+                    "id": 724,
+                    "providerId": 10,
+                    "name": "mit Backpflaumen gefüllter Schweinebraten,",
+                    "description": "dazu Rotkohl und Knödelscheiben",
+                    "price": 650,
+                    "averageRating": 3.5,
+                    "tags": [],
+                    "comments": []
+                },
+                {
+                    "id": 728,
+                    "providerId": 4,
+                    "name": "Pasta „Pomodori“",
+                    "description": "frische Tomaten, Parmesan, Olivenöl, Basilikum, Hühnchenbrust, dazu Nudeln",
+                    "price": 520,
+                    "averageRating": null,
+                    "tags": [
+                        "Tagessuppe"
+                    ],
+                    "comments": []
+                },
+                {
+                    "id": 733,
+                    "providerId": 4,
+                    "name": "Präsidentensuppe",
+                    "description": "Rinderhack, Tomaten, Sauerkraut, saure Gurken, Tomatenmark, wahlweise + Schmand",
+                    "price": 520,
+                    "averageRating": null,
+                    "tags": [],
+                    "comments": []
+                }
+            ]
+        }
     },
     initProviders: async () => {
-        var x = await request.get(webserver + 'getProvider/?location=["' + location + '"]');
-        providers = JSON.parse(x.data);
-//        providers = [
-//            {
-//                "id": 1,
-//                "name": "Schweinestall",
-//                "location": "Neubrandenburg",
-//                "url": "https://www.schweinestall-nb.de/mittagstisch-2/"
-//            },
-//            {
-//                "id": 2,
-//                "name": "Hotel am Ring",
-//                "location": "Neubrandenburg",
-//                "url": "http://www.hotel-am-ring.de/restaurant-rethra.html"
-//            },
-//            {
-//                "id": 3,
-//                "name": "AOK Cafeteria",
-//                "location": "Neubrandenburg",
-//                "url": "https://www.tfa-bistro.de"
-//            },
-//            {
-//                "id": 4,
-//                "name": "Suppenkulttour",
-//                "location": "Neubrandenburg",
-//                "url": "https://www.suppenkult.com/wochenplan.html"
-//            },
-//            {
-//                "id": 8,
-//                "name": "Das Krauthof",
-//                "location": "Neubrandenburg",
-//                "url": "https://www.daskrauthof.de/karte"
-//            },
-//            {
-//                "id": 10,
-//                "name": "Phoenixeum",
-//                "location": "Neubrandenburg",
-//                "url": "https://www.suppenkult.com/wochenplan.html"
-//            }
+        try{
+            var x = await request.get(webserver + 'getProvider/?location=["' + location + '"]');
+            providers = JSON.parse(x.data);
+        }
+        catch{
+            providers = [
+                {
+                    "id": 1,
+                    "name": "Schweinestall",
+                    "location": "Neubrandenburg",
+                    "url": "https://www.schweinestall-nb.de/mittagstisch-2/"
+                },
+                {
+                    "id": 2,
+                    "name": "Hotel am Ring",
+                    "location": "Neubrandenburg",
+                    "url": "http://www.hotel-am-ring.de/restaurant-rethra.html"
+                },
+                {
+                    "id": 3,
+                    "name": "AOK Cafeteria",
+                    "location": "Neubrandenburg",
+                    "url": "https://www.tfa-bistro.de"
+                },
+                {
+                    "id": 4,
+                    "name": "Suppenkulttour",
+                    "location": "Neubrandenburg",
+                    "url": "https://www.suppenkult.com/wochenplan.html"
+                },
+                {
+                    "id": 8,
+                    "name": "Das Krauthof",
+                    "location": "Neubrandenburg",
+                    "url": "https://www.daskrauthof.de/karte"
+                },
+                {
+                    "id": 10,
+                    "name": "Phoenixeum",
+                    "location": "Neubrandenburg",
+                    "url": "https://www.suppenkult.com/wochenplan.html"
+                }
+            ]
+        }
     },
     getOffers: () => {
         return offers;
